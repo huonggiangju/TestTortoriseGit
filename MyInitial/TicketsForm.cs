b18fefab98fb14b778103f957c8ff66d98baf362
GiangNguyen <nhgiang9992@gmail.com>
2022-03-07 13:19:10 +1030

    Initial loading MyInitial

---
+++ MyInitial/TicketsForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ticketing
+{
+    public partial class TicketsForm : Form
+    {
+        TicketPrice mTicketPrice;
+        int mSection = 2;
+        int mQuantity = 0;
+        bool mDiscount = false;
+
+        public TicketsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            mQuantity = int.Parse(txtQuantity.Text);
+
+            if (chkDiscount.Checked)
+                { mDiscount = true; }
+
+            if (radBalcony.Checked)
+                { mSection = 1; }
+            if (radGeneral.Checked)
+                { mSection = 2; }
+            if (radBox.Checked)
+                { mSection = 3; }
+
+            mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
+
+            mTicketPrice.calculatePrice();
+            lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TicketsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "TicketsForm";
+            this.Load += new System.EventHandler(this.TicketsForm_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void TicketsForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
